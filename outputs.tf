06d3832159e6f0e46e1ff6ef03e7531dd97d0611
Tafari Beckford <63197930+TafariBeckford@users.noreply.github.com>
2022-07-15 19:32:38 -0500

    remove extra server and refactor outputs

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,12 @@
-output "hello-world" {
-  description = "Print a Hello World text output"
-  value       = "Hello World"
+
+output "public_ip_web_app" {
+  value = aws_instance.web_server.public_ip
 }
 
-output "vpc_id" {
-  description = "Output the ID for the primary VPC"
-  value       = aws_vpc.vpc.id
+output "public_dns_web_app" {
+  value = aws_instance.web_server.public_dns
 }
 
-output "public_url" {
-  description = "Public URL for our Web Server"
-  value       = "https://${aws_instance.web_server.private_ip}:8080/index.html"
+output "environment" {
+value = var.environment
 }
-
-output "vpc_information" {
-  description = "VPC Information about Environment"
-  value       = "Your ${aws_vpc.vpc.tags.Environment} VPC has an ID of ${aws_vpc.vpc.id}"
-}
-
